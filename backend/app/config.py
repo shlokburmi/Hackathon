d178295cabcbbfe77562b1ab08d9476d96bef54d
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2025-11-15 22:21:26 +0530

    sd

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    MONGO_URL = os.getenv("MONGO_URL")
+    MONGO_URL = os.getenv("MONGO_URI") 
     DB_NAME = os.getenv("DB_NAME")
     SECRET_KEY = os.getenv("SECRET_KEY")
 
